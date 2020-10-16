41c13a5e82c2d81c4afcc3487263a3f237f4f6f6
cjk <colinjkim1997@gmail.com>
2020-10-16 17:26:20 +0900

    2pointer approach

---
+++ containerMostWater.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        curMax = 0
+        L = 0
+        R = len(height)-1
+        tempMax = 0
+        while (L<R):
+            left = height[L]
+            right = height[R]
+            w = R-L
+            if left<right:
+                tempMax = left * w
+                L+=1
+            else:
+                tempMax = right*w
+                R-=1
+            if tempMax > curMax:
+                curMax = tempMax
+        return curMax
+                
+        
